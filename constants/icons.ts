48f4f3f1b165bc87cf85d6a8037f290e20f0d1c8
Quinxie51 <quyendoan22@augustana.edu>
2025-04-14 23:45:38 +0200

    add constants for icon assets, change name to some icon assets

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,1 +1,15 @@
-// TODO: Quyen
+import books from "@/assets/icons/Books.png";
+import calendar from "@/assets/icons/Calendar.png";
+import home_page from "@/assets/icons/Home_Page.png";
+import speech_bubble from "@/assets/icons/Speech_Bubble.png";
+import topic from "@/assets/icons/Topic.png";
+import user_icon_female from "@/assets/icons/user_icon_female.png";
+
+export default {
+  books,
+  calendar,
+  home_page,
+  speech_bubble,
+  topic,
+  user_icon_female,
+};
