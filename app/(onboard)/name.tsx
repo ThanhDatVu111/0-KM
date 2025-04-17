3fa4c0ebb5bd12d38f4570767a50051fa086e1de
Quinxie51 <quyendoan22@augustana.edu>
2025-04-18 00:35:53 +0200

    change layout of name to match with sign up

--- app/(onboard)/name.tsx
+++ app/(onboard)/name.tsx
@@ -1,26 +1,43 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
 import React, { useState } from 'react';
+import AuthLayout from '@/components/AuthLayout';
+import FormInput from '@/components/FormInput';
 
 export default function NameScreen() {
   const router = useRouter();
   const [fullName, setFullName] = useState('');
 
   return (
-    <View className="flex-1 justify-center items-center bg-white px-6">
-      <Text className="text-xl font-bold mb-4">What's your name?</Text>
-      <TextInput
-        value={fullName}
-        onChangeText={setFullName}
-        placeholder="Enter your name"
-        className="border border-gray-300 rounded-lg px-4 py-3 w-full mb-4"
-      />
-      <TouchableOpacity
-        className="bg-accent py-3 rounded-lg w-full items-center"
-        onPress={() => router.push('./birthday')}
-      >
-        <Text className="text-white text-base">Next</Text>
-      </TouchableOpacity>
-    </View>
+    <AuthLayout activeTab="sign-up" onTabChange={(tab) => console.log(tab)}>
+      <View className="w-[300px]">
+        <Text
+          className="text-xl text-accent mb-2"
+          style={{ fontFamily: 'Poppins-Bold' }}
+        >
+          What's your name?
+        </Text>
+
+        <FormInput
+          label="Your Name"
+          borderColor="#F5829B"
+          value={fullName}
+          placeholder="Kevin Nguyen"
+          onChangeText={setFullName}
+        />
+
+        <TouchableOpacity
+          className="bg-accent py-3 rounded-lg w-full items-center mt-6"
+          onPress={() => router.push('/birthday')}
+        >
+          <Text
+            className="text-white text-[16px]"
+            style={{ fontFamily: 'Poppins-Regular' }}
+          >
+            Next
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </AuthLayout>
   );
 }
