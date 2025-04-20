82d351462ba626a2270b5ad22bf08079b8371171
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-04-20 13:03:00 -0700

    Add new room table migration to test DB upload process

---
+++ supabase/migrations/20250420195130_add_room_table.sql
@@ -0,0 +1,19 @@
+-- ROOM
+CREATE TABLE room (
+  room_id    TEXT    PRIMARY KEY    NOT NULL DEFAULT gen_random_uuid()::text,
+  user_1     TEXT                    NOT NULL REFERENCES users(user_id),
+  user_2     TEXT                    NULL      REFERENCES users(user_id),
+  created_at TIMESTAMP               NOT NULL DEFAULT now(),
+  filled     BOOLEAN                 NOT NULL DEFAULT FALSE
+);
+
+-- enforce one room per unordered pair
+CREATE UNIQUE INDEX one_room_per_pair
+  ON room (
+    LEAST(user_1, user_2),
+    GREATEST(user_1, user_2)
+  );
+
+-- By applying UNIQUE(LEAST(...), GREATEST(...)), you make sure the unordered pair {A,B} 
+-- can only ever exist once. If A tries to “join” B’s room after B already joined A’s, 
+-- they’ll get the same room rather than spinning up a new one.
