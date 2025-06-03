3574a46fa3af3abd154176361d07f639fbd4ca0c
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-06-02 19:03:33 -0700

    add API endpoints for updating and deleting entries - Added PUT endpoint for updating entries. - Added DELETE endpoint for removing entries. - Improved error handling for entry-related operations.

--- backend/src/routes/entriesRoutes.ts
+++ backend/src/routes/entriesRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { createEntries, fetchEntries } from '../controllers/entriesController';
+import { createEntries, deleteEntries, fetchEntries, updateEntries } from '../controllers/entriesController';
 
 const router = Router();
 
 router.get('/:book_id', fetchEntries); // Route to fetch entries by book_id
 router.post('/:book_id', createEntries); // Create a new entry
+router.delete('/:book_id/:entry_id', deleteEntries); // delete an entry
+router.put('/:book_id/:entry_id', updateEntries); 
 
 export default router;
