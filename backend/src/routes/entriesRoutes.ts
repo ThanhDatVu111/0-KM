df024bc99e33f07264eef81c502b08ee15a29cab
Thanh Dat Vu <thanhdatvu.203@gmail.com>
2025-06-22 15:21:28 -0400

    remove multer

--- backend/src/routes/entriesRoutes.ts
+++ backend/src/routes/entriesRoutes.ts
@@ -8,17 +8,11 @@ import {
   updateEntries,
 } from '../controllers/entriesController';
 
-const upload = multer({ storage: multer.memoryStorage() });
-// Multer prepares the req.files array so the controller (e.g., createEntries or updateEntries) 
-// can process the uploaded files and handle the logic for uploading them to S3.
 const router = Router();
 
 router.get('/:book_id', fetchEntries);
-
-router.post('/:book_id', upload.array('mediaFiles', 16), createEntries);
-
+router.post('/:book_id', createEntries);
 router.delete('/:book_id/:entry_id', deleteEntries);
-
-router.put('/:book_id/:entry_id', upload.array('mediaFiles', 16), updateEntries);
+router.put('/:book_id/:entry_id', updateEntries);
 
 export default router;
