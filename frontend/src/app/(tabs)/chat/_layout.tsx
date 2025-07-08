61e7dcbdb73e72a24f21fd00fec6cce3fb0002fd
Chau Hoang Minh Tran <chtran@umass.edu>
2025-07-08 02:45:32 -0400

    Minor update

--- frontend/src/app/(tabs)/chat/_layout.tsx
+++ frontend/src/app/(tabs)/chat/_layout.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Stack } from 'expo-router';
-import { SocketProvider } from 'utils/SocketProvider';
 import { MenuProvider } from 'react-native-popup-menu';
-import { QueryProvider } from 'utils/QueryProvider';
 
 export default function ChatLayout() {
   return (
